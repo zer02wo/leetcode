52ccc6a3a904c58adccfd14a94bf0acd1912e8bf
zer02wo <stephenstubbsbeta@gmail.com>
2025-04-27 09:28:08 +0100

    [383] First submission

---
+++ problems/383-ransom-note.js
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/ransom-note/description
+// tags: easy
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    // quick thoughts:
+        // 1. magazine can be longer than ransomNote, but not vice versa
+        // 2. only dealing with lowercase English letters, which means O(26) constant time potential
+        // 3. we only need to return a boolean, which could save some space complexity
+
+    if (magazine.length < ransomNote.length) {
+        return false;
+    }
+
+    // difficult to glean from the examples, but no constraints mention order, so we will assume this is unordered
+    // in which case a character frequency map probably makes the most sense
+
+    const charMap = new Map();
+
+    for (const char of magazine) {
+        if (!charMap[char]) {
+            charMap[char] = 0;
+        }
+
+        charMap[char] += 1;
+    }
+
+    // reduce count from map, early return if negative count or no key at all
+    for (const char of ransomNote) {
+        if (!charMap[char]) {
+            return false;
+        }
+
+        charMap[char] -= 1;
+
+        if(charMap[char] < 0) {
+            return false;
+        }
+    }
+
+    // has required letters for construction
+    return true;
+
+    // O(r + m) -> O(n) time complexity
+    // O(26) -> space complexity
+
+    // TODO: 25ms runtime, beats 17.25% -> how can we optimise?
+};
