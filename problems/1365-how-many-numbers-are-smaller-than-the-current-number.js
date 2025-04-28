14f4db18c6dc27b8d1f0c71f6c2477aba6f15a54
zer02wo <stephenstubbsbeta@gmail.com>
2025-04-28 09:03:50 +0100

    [1365] Frequency Map submission

--- problems/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ problems/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -5,6 +5,38 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+var smallerNumbersThanCurrentHashMap = function(nums) {
+    // lets try the frequency map approach
+    const numFreq = new Map();
+    const output = [];
+
+    // build frequency map
+    for (const num of nums) {
+        // usually I do a condition to handle the assignment, but trying out this pattern
+            // (even if I think it's less readable)
+        numFreq.set(num, (numFreq.get(num) || 0) + 1);
+    }
+
+    // get counts to build output
+    for (const num of nums) {
+        let count = 0;
+
+        // still nested loops, but not O(n^2) due to the constant (O(1)) 0 -> 100 num constraint
+        for (let i = 0; i < num; i++) {
+            if (numFreq.has(i)) {
+                count += numFreq.get(i);
+            }
+        }
+
+        output.push(count);
+    }
+
+    return output;
+
+    // this solution is actually *slower* than brute forcing: 13ms / beats 16.86%
+    // if O(100) > O(n) this makes sense, but I cannot confirm as test cases are hidden
+};
+
 var smallerNumbersThanCurrentBruteForce = function(nums) {
     // quick thoughts:
         // originally thought a Set could be useful, but we also need to count duplicates
