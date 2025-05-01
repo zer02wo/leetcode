a314326073f7a23675341151c81b931fbad0a4b6
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-01 20:36:58 +0100

    [1419] Walkthrough solution

--- problems/1419-minimum-number-of-frogs-croaking.js
+++ problems/1419-minimum-number-of-frogs-croaking.js
@@ -5,7 +5,72 @@
  * @param {string} croakOfFrogs
  * @return {number}
  */
-var minNumberOfFrogs = function(croakOfFrogs) {
+var minNumberOfFrogs = function (croakOfFrogs) {
+    // following someone else's approach for learning purposes
+
+    // we know a fully formed croak requires 5 characters
+        // so the length of croakOfFrogs must be wholly divisible by 5
+    if (croakOfFrogs.length % 5 !== 0) {
+        return -1;
+    }
+
+    // we know:
+        // c is the start/first stage of a new croak
+        // k is the end/last stage of an existing croak
+        // each subsequent letter in the croak should never be greater than the former
+
+    // counter aray keeps track of how many frogs at each stage of croaking (i.e. c -> k)
+        // increment the current stage (i.e. the frog has just started it)
+        // decrement the previous stage (i.e. the frog has moved on from it)
+            // if any stage has a negative value, this means the croak is out of order
+    const croakStages = [0,0,0,0,0];
+    // map the indexes to relevant characters
+    const croakMap = {c:0, r:1, o:2, a:3, k:4};
+
+    // we are interested in the maximum concurrency
+        // i.e. greatest value of croakStages[0] at any given point
+        // this will always be highest when the current stage = 'c'
+    let maxCroaks = 0;
+    // if any currentCroaks !== 0 after finishing iteration, then we have incomplete croak(s)
+    let currentCroaks = 0;
+
+    for (const stage of croakOfFrogs) {
+        let stageIndex = croakMap[stage];
+
+        croakStages[stageIndex]++;
+
+        if (stageIndex === 0) {
+            // 'c' -> start of a new croak, check for max
+            currentCroaks++;
+            maxCroaks = Math.max(maxCroaks, currentCroaks);
+        } else {
+            // 'r,o,a,k' -> decrement previous stage
+                // Note: -- at the start to return the subtracted value
+            let previous = --croakStages[stageIndex-1];
+
+            // croaking not in order
+            if (previous < 0) {
+                return -1;
+            }
+
+            if (stageIndex === 4) {
+                // 'k' -> end of an existing croak
+                currentCroaks--;
+            }
+        }
+    }
+
+    if (currentCroaks > 0) {
+        return -1;
+    } else {
+        return maxCroaks;
+    }
+
+    // 16ms / beats 45.4%
+    // surprised this isn't much faster than mine, but it is a lot cleaner & more extensible
+}
+
+var minNumberOfFrogsUnoptimal = function(croakOfFrogs) {
     // this question is pretty tough, so I only have one immediate intuition:
         // the string is invalid if there are not equal counts of: c, r, o, a, k
     // this lets us know when to return -1, but not whether frogs are consecutive or simultaneous
