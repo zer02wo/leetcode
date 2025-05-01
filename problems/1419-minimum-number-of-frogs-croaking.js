a5e236dcc31d70c042f2520ba760bf4769e99a91
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-01 19:46:23 +0100

    [1419] Dirty check for order

--- problems/1419-minimum-number-of-frogs-croaking.js
+++ problems/1419-minimum-number-of-frogs-croaking.js
@@ -23,8 +23,39 @@ var minNumberOfFrogs = function(croakOfFrogs) {
         ['k', 0],
     ]);
 
+    let croakCount = 1;
+
     for (const sound of croakOfFrogs) {
         croakMap.set(sound, croakMap.get(sound) + 1);
+
+        // Order matters as well - i.e. there cannot be more of 'r,o,a,k' than there is of 'c' at any time
+            // e.g. cannot be less of 'o,a,k' than there is of 'r' at any time, etc
+        // TODO: There has to be a more elegant way to write this
+        const cCount = croakMap.get('c');
+        const rCount = croakMap.get('r');
+        const oCount = croakMap.get('o');
+        const aCount = croakMap.get('a');
+        const kCount = croakMap.get('k');
+
+        if (cCount < rCount || cCount < oCount || cCount < aCount || cCount < kCount) {
+            return -1;
+        }
+
+        if (rCount < oCount || rCount < aCount || rCount < kCount) {
+            return -1;
+        }
+
+        if (oCount < aCount || oCount < kCount) {
+            return -1;
+        }
+
+        if (aCount < kCount) {
+            return -1;
+        }
+
+        // TODO: we know there are multiple frogs if we see a 'c' before seeing a 'k'
+            // i.e. another croak has started, before the last has finished
+            // TODO: Increment croakCount by difference between c & k?
     }
 
     const expectedCount = croakOfFrogs.length / 5;
@@ -34,6 +65,5 @@ var minNumberOfFrogs = function(croakOfFrogs) {
         }
     }
 
-    // TODO: Figure out simultaneous croaking
-    return 1;
+    return croakCount;
 };
