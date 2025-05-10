a45827ac03c75ce50d0dda216aacfce3b02619af
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-10 10:06:21 +0100

    [1108] Array build approach

--- problems/1108-defanging-an-ip-address.js
+++ problems/1108-defanging-an-ip-address.js
@@ -1,11 +1,37 @@
 // https://leetcode.com/problems/defanging-an-ip-address/description/
 // tags: easy, leetle
 
+// suspiciously simple question, but maybe some interesting insights gained about performance of operations
+// but I also suspect there is a lot of possible variance within the measurements
+
 /**
  * @param {string} address
  * @return {string}
  */
 var defangIPaddr = function(address) {
+    // trying array solution to compare to string solution
+
+    let output = [];
+
+    for (const digit of address) {
+        if (digit === '.') {
+            output.push('[.]');
+        } else {
+            output.push(digit);
+        }
+    }
+
+    return output.join('');
+
+    // 51 ms / beats 5.13%
+    // I would've expected this to have been faster than the string
+    // but string concatenation must be highly optimised in the native code
+    // perhaps this would be better for other languages with immutable strings (e.g. Java)
+
+    // or perhaps the run-to-run variance makes comparison pretty irrelevant
+};
+
+var defangIPaddrRebuildString = function(address) {
     // trying a solution without built-ins as it feels like cheating
     // strings in JS are immutable, so we'll need to create our own new string
         // TODO: Would an array be faster than string concatenation?
