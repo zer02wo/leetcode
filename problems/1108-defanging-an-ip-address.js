e85881779655f2d569f9a950cf2e7053da6960b7
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-10 09:50:13 +0100

    [1108] Array operations solution

--- problems/1108-defanging-an-ip-address.js
+++ problems/1108-defanging-an-ip-address.js
@@ -6,6 +6,14 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
+    return address.split('.').join('[.]');
+
+    // 52 ms / beats 5.13%
+    // I'm surprised this is *that* much worse
+    // but I suppose String.replaceAll() is incredibly optimised
+};
+
+var defangIPaddrReplaceAll = function(address) {
     return address.replaceAll('.', '[.]');
 
     // 32 ms / beats 92.59%
@@ -13,4 +21,5 @@ var defangIPaddr = function(address) {
     // but I don't see any constraints dictating why this isn't allowed
 
     // TODO: would array operations be faster? this isn't exactly *slow*
+    // TODO: I would be surprised if regex is faster, given this is a single character check?
 };
