5a3b6135e04f66a500f5a4671cbab4a87fcd6345
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-11 09:29:33 +0100

    [191] Modulo/remainder solution

---
+++ problems/191-number-of-1-bits.js
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/number-of-1-bits/
+// tags: easy, leetle
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let setBits = 0;
+
+    // I vaguely remember converting between bases requires the use of division/modulo
+        // ... figured it out pretty quick
+    while (n > 0) {
+        // because we are in binary, remainder will only be 1 or 0 (truthy/falsy)
+        if (n % 2) {
+            setBits++;
+        }
+
+        n = Math.floor(n / 2);
+    }
+
+    return setBits;
+
+    // 1 ms / beats 31.78% (first run)
+    // 0 ms / beats 100.0% (second run)
+    // TODO: There's probably a bitwise operation that works here as well
+        // obviously this is already fast enough, but interested to learn
+        // because we're working with numbers from 1 to 2^31 - 1, that means 32 bits
+};
+
+// Objective: take a base 10 number and count the number of set bits in its binary representation
+    // I.e. how many 1's in a binary number
+    // E.g. 11 = 1011, 3 set bits
+    // E.g. 128 = 10000000, 1 set bit
+// Approach: I see two parts:
+    // 1. Convert the base 10 number to binary
+    // 2. Count the instances of 1 in the binary
+// It seems very possible to do both parts together
+
+// Example base conversions (to refresh my memory):
+    // 11 / 2 = 5 r 1
+    // 5 / 2 = 2 r  1
+    // 2 / 2 = 1 r  0
+    // 1 / 2 = 0 r  1
+
+    // 128 / 2 = 64 r   0
+    // 64 / 2 = 32 r    0
+    // 32 / 2 = 16 r    0
+    // 16 / 2 = 8 r     0
+    // 8 / 2 = 4 r      0
+    // 4 / 2 = 2 r      0
+    // 2 / 2 = 1 r      0
+    // 1 / 2 = 0 r      1
