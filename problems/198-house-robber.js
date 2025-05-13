081f65089c53a71ab4b0eaaddccaa1965d2dc2ea
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-13 19:13:41 +0100

    [198] Current/next pointers idea

--- problems/198-house-robber.js
+++ problems/198-house-robber.js
@@ -5,6 +5,31 @@
  * @param {number[]} nums
  * @return {number}
  */
+var robOversimplifiedAlt = function(nums) {
+    let lootSum = 0;
+    let i = 0;
+
+    while (i < nums.length) {
+        const current = nums[i];
+        const next = nums[i+1];
+
+        if (current > next) {
+            lootSum += current;
+            i += 2;
+        } else {
+            lootSum += next;
+            i += 3;
+        }
+    }
+
+    return lootSum;
+
+    // TODO: this is too greedy and doesn't consider the long-term
+        // i.e. this works for [2,1,1,2]
+        // but not for [1,2,3,1]
+    // TODO: We need some kind of method of looking back as well
+}
+
 var robOversimplified = function(nums) {
     let evenSum = 0;
     let oddSum = 0;
@@ -39,3 +64,16 @@ var robOversimplified = function(nums) {
 
 // this seems to just be comparing the sum of the even indexes against the sum of the odd indexes?
     // this feels way too simple of a solution for a medium, so I am probably missing some constraint/complexity?
+
+// notes continued after initial attempt:
+// Example: nums = [2,1,1,2]    = 4
+    // I.e. (2 + 2 = 4) > (2 + 1 = 3)
+
+// as we can't have adjacent numbers, should we deal with the numbers as pairs?
+// i.e. for the example above, separate [2, 1] and [1, 2] into pairs and then find the maximum (valid) sum between the two?
+    // this still feels like a greedy approach that could result in a local maxima only
+// "pairs" might not be a valid categorisation, but rather looking ahead at the next two non-adjacent houses from the current house
+
+// we could find the global maxima element and work from there
+    // again this is a greedy approach that only works if one house has an especially higher value than the others
+    // e.g. this would fail for nums = [99,100,99,1]
