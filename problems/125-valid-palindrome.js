5d3fa2220631ec965f2af64e76ea3583de052b1c
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-21 08:35:19 +0100

    [125] Normalise before iteration solution

--- problems/125-valid-palindrome.js
+++ problems/125-valid-palindrome.js
@@ -35,10 +35,45 @@ var isPalindrome = function(s) {
 
     return true;
 
-    // 3 ms / beats 85.88%
+    // 3 ms / beats 85.88% (first run)
+    // 13 ms / beats 11.07% (second run) ???
+    // 8 ms / beats 26.37% (third run) ??
+        // not sure if there is just a lot of variance in this particular problem,
+        // or if I got very lucky on my first run
+
     // O(n) time complexity / O(1) space complexity
     // TODO: would it be faster to sanitise/normalise the string first?
+        // ANS: no, see below
 };
+
+var isPalindromeNormaliseFirst = function(s) {
+    // normalise string:
+    s = s.toLowerCase();
+    s = s.replace(/[^0-9a-z]/g, '');
+
+    let i = 0, j = s.length - 1;
+
+    while (i < j) {
+        // check characters are the same
+        if (s[i] !== s[j]) {
+            return false;
+        }
+
+        // bring pointers towards middle
+        i++;
+        j--;
+    }
+
+    return true;
+
+    // 7 ms / beats 35.86% (first run)
+    // 5 ms / betats 62.98% (second run)
+
+    // still O(n) time complexity, but in reality there will be more iterations over string 's'
+        // i.e. once to switch to lowercase, twice to remove non-alphanumeric characters
+    // previous solution was an "in-place" solution, but because JS strings are immutable
+        // toLowerCase and replace will re-create the string (even if we are overwriting it)
+}
 
 // palindrome: a string that is the same read normally/forwards as it is reversed/backwards
 // constraints:
@@ -52,3 +87,7 @@ var isPalindrome = function(s) {
 // palindrome is a classic two pointer problem
     // just remember to account for all non-alphanumeric characters
     // we could remove them all beforehand, but that would increase the runtime?
+
+// it's also possible to simply reverse the normalised string and compare,
+    // but in practice that would result in more "iterations"
+// it is probably faster when using the built-in methods
