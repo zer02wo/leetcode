10530e116c784cba6919b30b6e77012547a3bc4b
zer02wo <stephenstubbsbeta@gmail.com>
2025-05-21 15:05:43 +0100

    [151] Built-in methods solution

---
+++ problems/151-reverse-words-in-a-string.js
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/reverse-words-in-a-string/
+// tags: medium, strings
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+
+}
+
+var reverseWordsSimple = function(s) {
+    // split sentence into array of words, filtering out empty strings
+    const words = s.split(' ').filter(word => word);
+
+    // reverse array order and convert to string
+    return words.reverse().join(' ');
+
+    // 1 ms  / beats 83.81%
+    // as mentioned below this feels like it avoids the problem, rather than engaging with it
+    // TODO: implement a solution without using (only) built-in methods
+};
+
+// objective is to reverse the order of words in a sentence, not reverse the contents of words
+    // AKA almost Yoda-ifying the sentence
+
+// EXAMPLE: s = "  the  sky is    blue   "
+// OUTPUT: "blue is sky the"
+    // word order is reversed, but words themselves stay the same
+    // leading/trailing whitespace is expected to be removed
+
+// constraints:
+    // s will always contain *at least* one word
+    // s wcontains English letters (upper & lower), digits & spaces ' '
+
+// similar to leetcode #520, this seems like another relatively simple one if you rely on built-ins:
+    // trim the string
+        // edit: this is actually unnecessary because of filtering later on
+    // split the string on spaces
+    // remove empty strings (due to multiple spaces)
+    // reverse the array of words
+    // return array joined as string with spaces
+// see reverseWordsSimple()
+
+// but is that again a bit cheat-y/not in the spirit of the problem?
