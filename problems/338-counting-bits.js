dc2f878d2b34b2b4b186ba5e90256ccd152a1d0d
zer02wo <stephenstubbsbeta@gmail.com>
2025-06-02 20:48:47 +0100

    [338] Notes & optimise brute force initialisation

--- problems/338-counting-bits.js
+++ problems/338-counting-bits.js
@@ -1,14 +1,10 @@
 // https://leetcode.com/problems/counting-bits/
 // tags: easy, bit manipulation
 
-/**
- * @param {number} n
- * @return {number[]}
- */
-var countBits = function(n) {
-    let output = [];
+var countBitsBruteForce = function(n) {
+    const output = [0];
 
-    for (let i = 0; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         let num = i;
         let setBits = 0;
 
@@ -57,3 +53,35 @@ var countBits = function(n) {
         // convert to binary / count number of 1 bits
             // I normally do this by modulo/division
         // push to output array
+
+// it's obvious that we're doing repeated work calculating smaller binary numbers
+    // i.e. 7 = 0111
+        // 7 = 4 + 2 + 1
+        // :: 0100 + 0010 + 0001
+    // but how can we use this?
+// NOTE: had to look up a solution as this wasn't clicking after a long day.
+    // similar to what I noted earlier we need to recognise the relationship between significant bits & powers of 2
+    // 0 & 1 are represented by 1 bit (2^0)
+    // 2 & 3 are represented by 2 bits (2^1)
+    // 4 > 7 are represented by 3 bits (2^2)
+    // 8 > 15    represented by 4 bits (2^3)
+    // 16 ... etc (2^4)
+
+    // we create an *OFFSET* based on the most significant bit (highest power of 2 >= n)
+        // e.g. 8 = 1000, equivalent to 000 ignoring most significant bit (MSB)
+            // i.e. 8 - 8 = 0
+        // e.g. 9 = 1001, equivalent to 001 ignoring MSB
+            // i.e. 9 - 8 = 1
+        // e.g. 10= 1010, equivalent to 010 ignoring MSB
+            // i.e. 10 - 8 = 2
+        // e.g. 11= 1011, equivalent to 011 ignoring MSB
+            // i.e. 11 - 8 = 3
+        // e.g. 12= 1100, equivalent to 100 ignoring MSB
+            // i.e. 12 - 8 = 4 .. 4 - 4 = 0 (00)
+        // e.g. 13= 1101, equivalent to 101 ignoring MSB
+            // i.e. 13 - 8 = 5 .. 5 - 4 = 1 (01)
+        // e.g. 14= 1110, equivalent to 110 ignoring MSB
+            // i.e. 14 - 8 = 6 .. 6 - 4 = 2 (10)
+        // e.g. 15= 1111, equivalent to 110 ignoring MSB
+            // i.e. 15 - 8 = 7 .. 7 - 4 = 3 (11)
+        // etc.
