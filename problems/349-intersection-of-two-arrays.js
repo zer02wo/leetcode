7b7eb7ff9cb5ff84cabb9a9b55a4f4888287cb3d
zer02wo <stephenstubbsbeta@gmail.com>
2025-06-09 19:52:04 +0100

    [349] Native Set approach

--- problems/349-intersection-of-two-arrays.js
+++ problems/349-intersection-of-two-arrays.js
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/intersection-of-two-arrays/
+// tags: easy, leetle, array
 
 /**
  * @param {number[]} nums1
@@ -37,3 +38,31 @@ var intersection = function(nums1, nums2) {
 
     return intersect;
 };
+
+// revisiting problem:
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersectionNativeSet = function(nums1, nums2) {
+    var set1 = new Set(nums1);
+    var set2 = new Set(nums2);
+
+    return Array.from(set1.intersection(set2));
+
+    // 1 ms / beats 82.71%
+};
+
+// find intersection of two arrays
+    // definition: set of elements present in both arrays (i.e. the same as the Set operation)
+// each element in the result must be unique, but can be in any order
+
+// EXAMPLE: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// OUTPUT: [9,4] (or [4,9]
+    // i.e. only [9] and [4] are contained in both arrays
+
+// as mentioned above the easiest method will be Set1.intersection(Set2)
+    // but perhaps the question wants us to demonstrate how to do this without sets
+    // this might be worth visiting afterwards
