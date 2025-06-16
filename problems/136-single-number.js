08c51fc92b0a29430ec16495f90ecee20e680133
zer02wo <stephenstubbsbeta@gmail.com>
2025-06-16 09:18:25 +0100

    [136] Notes on additional approaches / theory

--- problems/136-single-number.js
+++ problems/136-single-number.js
@@ -65,3 +65,27 @@ var singleNumber = function(nums) {
     // i.e. all the pairs cancel each other out...
     // n ^ 0 == n
         // leaving just the single number as the output
+// bitwise XOR works even when the numbers are not consecutive, e.g. [8,15,8]
+    //  8 ^ 0 = 1000 (8)
+    // 15 ^ 8 = 0111 (7)
+    //  7 ^ 8 = 1111 (15)
+        // i.e. 15 is the single number
+
+// without bit manipulation, you could:
+    // sort the array, then look for the number that does not appear twice consecutively
+        // O(n log n) + O(n) time complexity, O(n) space complexity
+            // or O(1) space complexity if the original array is overwritten
+    // use a HashMap could store the count of each number, return where count == 1
+        // O(n + m) time complexity, where m == size of HashMap (roughly n / 2), O(m) space complexity
+    // use a Set:
+        // add the number upon first appearance
+        // remove the number upon second appearance
+            // this would leave only the single number in the Set
+            // essentially the same logic behind the bitwise XOR
+        // (same approach could be done with the HashMap actually to improve performance)
+        // O(n) time complexity, O(n) space complexity
+    // we don't have a known range, so we can't use a (Gauss) sum like leetcode #268
+
+// however the question *requires* O(n) runtime + O(1) extra space
+    // which I don't think is achievable with anything other than the bitwise XOR approach
+    // (unless you instantiate a huge array of a fixed size, which isn't practical)
