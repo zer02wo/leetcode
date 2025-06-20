c7d3049cb6033b53f08e8e0a3b0640fbe0fc90f1
zer02wo <stephenstubbsbeta@gmail.com>
2025-06-20 08:58:09 +0100

    [303] PrefixSum revisited solution

--- problems/303-range-sum-query-immutable.js
+++ problems/303-range-sum-query-immutable.js
@@ -69,3 +69,62 @@ NumArray.prototype.sumRange = function(left, right) {
         // i.e. [0+1+2+3+4+5] - [0+1] = [2+3+4+5] :: which is equivalent to range (2,5)
     // NOTE: because we want to be inclusive of left index (2), we cannot subtract this from the sum
         // so this is more accurately: NumArray.sumRange(0,5) - NumArray.sumRange(0,(2-1))
+
+
+
+// revisiting problem:
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ *
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    // cumulative sum so far
+    let totSum = nums[0];
+
+    // initialise to 0th element, as cannot be summed with anything yet
+    this.prefixSum = [totSum];
+
+    // calculate cumulative sum at each index
+    for (let i = 1; i < nums.length; i++) {
+        totSum += nums[i];
+        this.prefixSum.push(totSum);
+    }
+};
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    // account for 0th left index
+        // i.e. there is no previous index to be inclusive of, so we subtract nothing
+    if (left === 0) {
+        return this.prefixSum[right];
+    }
+
+    // sumRange(l,r) = sumRange(0,r) - sumRange(0,l-1)
+    return this.prefixSum[right] - this.prefixSum[left - 1];
+
+    // 7 ms / beats 56.44%
+
+    // NumArray constructor is an O(n) operation
+    // each subsequent call of NumArray.sumRange() is O(1)
+        // without the PrefixSum it would be O(n) to calculate sumRange() each time
+};
+
+// the fundamental intuition/principal of this problem is that:
+    // sumRange(i,j) = sumRange(0,j) - sumRange(0,i-1)
+        // i-1 because we want to be inclusive of i
+// i.e. PrefixSum: calculate the cumulative sum from 0 to i
+
+// EXAMPLE: [-2,0,3,-5,2,-1]
+// sumRange(2,5) = [3+(-5)+2+(-1)] =            -1
+    // sumRange(0,1) = [(-2)+0] =               -2
+    // sumRange(0,5) = [(-2)+0+3+(-5)+2+(-1)] = -3
+
+    // therefore: -3 - (-2) = -3 + 2 = -1
