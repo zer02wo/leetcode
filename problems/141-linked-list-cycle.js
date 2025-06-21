d1388d3e01930b1d9cf62d69c2e7b028a97f78c7
zer02wo <stephenstubbsbeta@gmail.com>
2025-06-21 11:52:47 +0100

    [141] O(n) memory solution & additional notes

--- problems/141-linked-list-cycle.js
+++ problems/141-linked-list-cycle.js
@@ -14,6 +14,11 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+    // can't have cycles if there is no list
+    if (!head) {
+        return false;
+    }
+
     // keep reference to all the nodes we've seen
     const nodeSet = new Set();
     // pointer for current node
@@ -33,7 +38,7 @@ var hasCycle = function(head) {
 
     return false;
 
-    // TODO: fails for test case: head = []
+    // 54 ms / beats 47.37%
 };
 
 // given head of singly linked list, determine if the linked list has a cycle in it
@@ -43,6 +48,17 @@ var hasCycle = function(head) {
 // internally this problem uses a `pos` variable to denote the index of the node that the tail points to
     // this is not a parameter
 
+// EXAMPLE: 3 > 2 > 0 -4 -¬
+//              ^---------|
+    // here we see that the tail links back to an existing node,
+    // internally pos = 1 (2nd node)
+
+// constraints:
+    // number of nodes  in range [0, 10^4]
+        // i.e. *will* need to check for empty linked list
+    // -10^5 <= Node.val <= 10^5
+    // pos is -1 or valid index in the linked list
+        // i.e. either no cycle or valid cycle in linked list
 
 // this seems pretty trivial to do using O(n) space complexity,
 // just by creating a Set of nodes and checking if one is repeated
