4ed749ea51f46e1e02fa6e0ad015a8d037e69bad
zer02wo <stephenstubbsbeta@gmail.com>
2025-06-22 17:25:17 +0100

    [1502] Set with max/min values solution

--- problems/1502-can-make-arithmetic-progression-from-sequence.js
+++ problems/1502-can-make-arithmetic-progression-from-sequence.js
@@ -6,6 +6,36 @@
  * @return {boolean}
  */
 var canMakeArithmeticProgression = function(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+
+    // difference = max - min / arr.length-1
+    const difference = (max - min) / (arr.length - 1);
+
+    // create Set from array for O(1) value lookups
+    const lookup = new Set(arr);
+    let nthVal = min;
+
+    // increment from min -> max by difference
+    while (nthVal < max) {
+        nthVal += difference;
+
+        // if nthVal of arithmetic progression does not exist, this is invalid
+        if (!lookup.has(nthVal)) {
+            return false;
+        }
+    }
+
+    return true;
+
+    // 0 ms / beats 100%
+    // O(n) time complexity, O(n) space complexity
+
+    // took a bit of trial/error & maths to figure out the pattern
+    // then needed a hint about using a Set for looking up values
+};
+
+var canMakeArithmeticProgressionSorting = function(arr) {
     // sort the array to create consistent order
         // I can't think of a case where arithmetic progression would be possible using an unsorted array
     const sortedArr = arr.sort((a, b) =>  a - b);
@@ -56,3 +86,44 @@ var canMakeArithmeticProgression = function(arr) {
         // if any of the future consecutive elements do not obey this distance, there is no arithmetic progression
 
 // I can't think of an immediate pattern, so let's start with sorting ^
+
+// EXAMPLE: [1,3,5,7,9],    AP = 2 or -2
+    // 1 + 9 = 10
+    // 10 / 5 = 2
+// EXAMPLE: [2,5,8,11,14],  AP = 3 or -3
+    // 2 + 14 = 16
+    // 16 / 5 = 3.2
+// EXAMPLE: [4,8,12,16,20], AP = 4 or -4
+    // 4 + 20 = 24
+    // 24 / 5 = 4.8
+
+// there's definitely *some* pattern here as I'm getting close values,
+// but it's not as simple as what I've tried above...
+    // n0 + n1 + ... + n-1 + n
+    // n1 = n0 === n2 - n1
+
+// EXAMPLE: [1,3,5,7,9]
+    // 9 = 1 + (4 * 2)
+        // max = min + (x * difference)
+// EXAMPLE: [-2,1,4,7,10,13]
+    // 13 = -2 + (5 * 3)
+// EXAMPLE: [4,8,12,16,20]
+    // 12 = 4 + (4 * 4)
+        // max = min + ((arr.length-1) * difference)
+
+// therefore, rearranging:
+    // max - min = (arr.length-1) * difference
+    // max - min / (arr.length-1) = difference
+// so the formula is:
+    // (max - min) / arr.length - 1
+
+// 9 - 1 = 8
+    // 8 / (5-1) = 2
+// 14 - 2 = 12
+    // 12 / (5-1) = 3
+// 20 - 4 = 16
+    // 16 / (5-1) = 4
+
+// TODO: how can we use this difference to check without sorting..?
+    // we could use a Set to lookup expected values (i.e. min + (n * diff)) in O(1) time
+    // i.e. arithmetic progression = min, min + diff, min + 2*diff, min + 3*diff, ..., min + n-1*diff, max
