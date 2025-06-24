6a1aef44f6574f0788940f7e90f1c87ec1e7f97a
zer02wo <stephenstubbsbeta@gmail.com>
2025-06-24 08:44:55 +0100

    [206] Iterative solution

---
+++ problems/206-reverse-linked-list.js
@@ -0,0 +1,86 @@
+// https://leetcode.com/problems/reverse-linked-list/
+// tags: easy, linked list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    // keep track of previous node for reversing link
+    let prev = null;
+    // current node pointer
+    let node = head;
+    // keep track of next node for iteration
+    let next = null;
+
+    while (node) {
+        // get next node in linked list
+        next = node.next;
+
+        // point current node to previous node
+            // i.e. reverse link direction
+        node.next = prev;
+
+        // current node is now previous node
+        prev = node;
+        // next node is now current node
+        node = next;
+    }
+
+    return prev;
+
+    // 0 ms / beats 100%
+    // O(n) time complexity, O(1) space complexity
+
+    // TODO: problem suggests a recursive solution as well
+};
+
+// given head of singly linked list, reverse the list & return it
+
+// EXAMPLE: 1 > 2 > 3 > 4 > 5
+// OUTPUT: 5 > 4 > 3 > 2 > 1
+
+// constraints:
+    // number of nodes in range [0,5000]
+    // -5000 <= Node.val <= 5000
+
+// first thought: keep reference to previous node and override the Node.next pointer with the current node
+    // but we also need to keep reference to the current next node before we modify it, to continue iterating through list
+
+// EXAMPLE: 1 > 2 > 3 > 4
+// START
+// prev = null
+// node = 1
+// next = 2
+    // 1 > null
+    // 2 > 3 > 4
+
+// prev = 1
+// node = 2
+// next = 3
+    // 2 > 1
+    // 3 > 4
+
+// prev = 2
+// node = 3
+// next = 4
+    // 3 > 2 > 1
+    // 4
+
+// prev = 3
+// node = 4
+// next = null
+    // 4 > 3 > 2 > 1
+    // null
+
+// RETURN prev
+// END
+
+// we need a while loop to stop after current node is set to null
