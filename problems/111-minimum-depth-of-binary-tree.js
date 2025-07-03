e4cedffeda6fb94bd451f7c2fbe65dd829a702e6
zer02wo <stephenstubbsbeta@gmail.com>
2025-07-03 08:56:47 +0100

    [111] Initial problem description/thoughts

---
+++ problems/111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+// tags: easy, binary tree, depth-first search, breadth-first search
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+
+};
+
+// given the root of a binary tree, find its minimum depth:
+    // the number of nodes along the shortest path from the root node down to the nearest leaf node
+
+// EXAMPLE:
+//   3
+// 9   20
+//   15  7
+// OUTPUT: 2
+    // 3 -> 9
+    // i.e. in this question the root node counts as a depth of 1, instead of 0 like you would expect
+
+// constraints:
+    // number of nodes in tree in range [0,10^5]
+        // i.e. a null/empty check is required
+    // -1000 <= Node.val <= 1000
+
+// intuition: given that we're dealing with the depth of a node
+    // we want to use a depth-first search (DFS)
+    // implement using a stack (FILO) or recursion
+// although we could use a BFS approach to keep track of the level more simply
+// otherwise we would need to modify the stack to also keep track of the depth of each node
+    // i.e. a 2D array of [[node,depth],[node,depth]]
+// recursion may be a more intuitive solution in this case
+
+// let's start with BFS
