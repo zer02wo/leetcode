4f0c99b2842232c7f12cd7b77bc1f49c18b83a8a
zer02wo <stephenstubbsbeta@gmail.com>
2025-07-04 08:31:36 +0100

    [100] BFS iterative comparison solution

--- problems/100-same-tree.js
+++ problems/100-same-tree.js
@@ -15,7 +15,68 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
+    // both trees have 0 nodes
+    if (!p && !q) {
+        return true;
+    }
 
+    // one tree has 0 nodes, the other does not
+    if (!p || !q) {
+        return false;
+    }
+
+    // BFS data structure for both trees
+    let pQueue = [p];
+    let qQueue = [q];
+
+    while (pQueue.length || qQueue.length) {
+        let pSize = pQueue.length;
+        let qSize = qQueue.length;
+
+        // if trees are identical, number of nodes at each level should be the same
+        if (pSize !== qSize) {
+            return false;
+        }
+
+        for (let i = 0; i < pSize; i++) {
+            let pNode = pQueue.shift();
+            let qNode = qQueue.shift();
+
+            // values are not aligned, trees are not the same
+            if (pNode.val !== qNode.val) {
+                return false;
+            }
+
+            if (pNode.left && qNode.left) {
+                // both trees have left node, add to queues
+                pQueue.push(pNode.left);
+                qQueue.push(qNode.left);
+            } else if (pNode.left || qNode.left) {
+                // only one of the trees has a left node, not identical
+                return false;
+            }
+
+            if (pNode.right && qNode.right) {
+                // both trees have right node, add to queues
+                pQueue.push(pNode.right);
+                qQueue.push(qNode.right);
+            } else if (pNode.right || qNode.right) {
+                // only one of the trees has a right node, not identical
+                return false;
+            }
+        }
+    }
+
+    return true;
+
+    // 0 ms / beats 100%
+    // pretty happy with this solution, arrived at it very quickly
+        // albeit because I am using the same algorithm as the past couple of days
+        // but this is modified to need some additional checks
+    // my solution is probably a little verbose, I imagine there are more clean ways to do this
+        // need to not fall into the trap of doing what I'm familiar with
+        // TODO: maybe a DFS / following a single node would be sufficient?
+        // TODO: maybe a recursive solution would be more appropriate?
 };
 
 // given roots of two binary trees (p, q), check if they are the same:
