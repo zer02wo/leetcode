bfbf1d5059d5a6abf5fffe67e26b24c11837a1ad
zer02wo <stephenstubbsbeta@gmail.com>
2025-07-08 17:02:24 +0100

    [543] Recursive DFS helper solution

--- problems/543-diameter-of-binary-tree.js
+++ problems/543-diameter-of-binary-tree.js
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/diameter-of-binary-tree/
-// tags: easy, binary tree,
+// tags: easy, binary tree, depth-first search
 
 /**
  * Definition for a binary tree node.
@@ -14,13 +14,38 @@
  * @return {number}
  */
 var diameterOfBinaryTree = function(root) {
-    if (!root) {
-        return 0;
+    let diameter = 0;
+
+    dfs(root);
+
+    return diameter;
+
+    function dfs(node) {
+        if (!node) {
+            return 0;
+        }
+
+        // recursively search left/right subtree for longest path
+        const leftPath = dfs(node.left);
+        const rightPath = dfs(node.right);
+
+        // diameter = maxDepth(leftSubtree) + maxDepth(rightSubtree)
+        diameter = Math.max(diameter, leftPath + rightPath);
+
+        // maximum depth of subtrees + 1 for backtracking from current node
+        return Math.max(leftPath, rightPath) + 1;
     }
 
-    // TODO: this is accounting for *every* node in the tree, not just along the deepest path
-        // very quick attempt to build off previous solution for leetcode #104
-    return diameterOfBinaryTree(root.left) + diameterOfBinaryTree(root.right) + 1;
+    // 1 ms / beats 87.18%
+    // O(n) time complexity, O(h) space complexity (h = height of tree)
+
+    // great solution here: https://leetcode.com/problems/diameter-of-binary-tree/
+        // includes great/visual explanation here: https://www.youtube.com/watch?v=Tu-9JVF0Mz0
+
+    // definitely not an easy question
+        // would've really struggled to get this without a hint
+        // I had the initial logic/formula for the diameter,
+        // but tried to early/over-optimise without a helper function
 };
 
 // given root of binary tree, return the length of the diameter of the tree
@@ -40,7 +65,6 @@ var diameterOfBinaryTree = function(root) {
 
 // intuition: this problem *seems* to be able to simplify to:
     // maxDepth(leftSubtree) + maxDepth(rightSubtree)
-        // + 1 for root node
 // the question does state the path "may not" route through the root node,
     // but I'm not provided an example where this isn't the case
     // maybe a situation where e.g. a child node in the left subtree has two deep subtrees
