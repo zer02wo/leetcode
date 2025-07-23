7f030969bca0a20aed8dd14d1b13b0d029d3f0d1
zer02wo <stephenstubbsbeta@gmail.com>
2025-07-23 11:05:48 +0100

    [73] Constant memory marking approach/attempt

--- problems/73-set-matrix-zeroes.js
+++ problems/73-set-matrix-zeroes.js
@@ -5,6 +5,50 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+var setZeroesConstantMemory = function(matrix) {
+    // because we're using the first element of each row/column as the mark
+    // that means there's an overlap at [0,0],
+    // this additional boolean flag marks the first row separate to the first column
+    let isFirstRowZero = false;
+
+    // first pass: mark zeroes
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (matrix[row][col] === 0) {
+                if (row !== 0) {
+                    // not first row, mark row as usual
+                    matrix[row][0] = 0;
+                } else {
+                    // mark first row using additional bool flag
+                    isFirstRowZero = true;
+                }
+
+                // mark column as usual
+                matrix[0][col] = 0;
+            }
+        }
+    }
+
+    // second pass: set zeroes
+        // TODO: would this be better to do in 3 passes?
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (row === 0 && isFirstRowZero) {
+                matrix[row][col] = 0;
+                continue;
+            }
+
+            if (matrix[row][0] === 0 || matrix[0][col] === 0) {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
+    // TODO: fails for following test case:
+        // matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+        // TODO: seems to be because we're updating row/column 0
+};
+
 var setZeroes = function(matrix) {
     const zeroRows = new Set();
     const zeroCols = new Set();
@@ -39,8 +83,6 @@ var setZeroes = function(matrix) {
 
     // really happy with this solution and how quickly I arrived at it
     // even beats 88.27% on memory as well which is nice
-
-    // TODO: how to improve this to constant space?
 };
 
 // given an `m x n` integer matrix:
@@ -83,3 +125,13 @@ var setZeroes = function(matrix) {
 // this would be O(m + n) space complexity
 
 // TODO: the problem describes the possibility for a constant space solution
+    // this seems to suggest that we would need to use mark by negation pattern
+    // but given that the current value of any element is already any 32 bit signed integer value...
+        // how could we reliably mark this without *luck* that this value won't appear?
+        // we could use -0 as a value, as that *technically* isn't a real number?
+        // or we could use a string value, but that feels like it violates the constraints of the matrix
+// EDIT AFTER WATCHING EXPLANATION: https://www.youtube.com/watch?v=T41rL0L3Pnw
+    // we can use 0 to mark, as we're going to update to that value regardless
+        // clearly I was overcomplicating this a little bit
+    // we just need to keep the marks for rows and columns separate
+        // be aware of the overlap in cell [0,0] which will need additional (O(1)) memory
