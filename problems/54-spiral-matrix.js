d4e644ac1639447cdd7e3f8d5f7dea3df545a4c9
zer02wo <stephenstubbsbeta@gmail.com>
2025-07-23 13:25:09 +0100

    [54] Initial iterative direction approach

--- problems/54-spiral-matrix.js
+++ problems/54-spiral-matrix.js
@@ -6,7 +6,50 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
+    const output = [];
+    // start at [0,0]
+    let x = 0;
+    let y = 0;
+    // moving along positive x (i.e. right) to start
+    let dx = 1;
+    let dy = 0;
+    // define boundaries
+    let rowSize = matrix.length;
+    let colSize = matrix[0].length;
 
+    for (let i = 0; i < (rowSize * colSize); i++) {
+        console.log(x + ', '+ y);
+        // push element to output array
+        output.push(matrix[y][x]);
+
+        // check if coordinate will exceed any boundary in next move
+            // then change direction (i.e. rotate 90 degrees)
+        if (x + dx >= colSize) {
+            dx = 0;
+            dy = 1;
+        } else if (y + dy >= rowSize) {
+            dx = -1;
+            dy = 0
+        } else if (x + dx < 0) {
+            dx = 0;
+            dy = -1;
+        } else if (y + dy < 0) {
+            dx = 1;
+            dy = 0;
+        }
+
+        // moving position == coordinate + direction
+        // i.e. dir = 1 (positive), dir = 0 (stationary), dir = -1 (negative)
+        x += dx;
+        y += dy;
+    }
+
+    return output;
+
+    // TODO: failing due to not constraining boundaries
+        // E.g. for first example of 3x3 matrix (1 -> 9)
+        // This is returning to [0,0] instead of moving to [1,1] as we're not moving the boundary inwards
+        // TODO: may need a lower boundary for each direction as well?
 };
 
 // given `m x n` matrix, return all elements in "spiral" order
