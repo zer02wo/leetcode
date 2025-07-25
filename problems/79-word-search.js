3e48969d1bb8c7763a952f35a985dc5852961075
zer02wo <stephenstubbsbeta@gmail.com>
2025-07-25 16:08:13 +0100

    [79] Initial DFS approach attempt

--- problems/79-word-search.js
+++ problems/79-word-search.js
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/word-search/
-// tags: medium, array, matrix
+// tags: medium, array, matrix, depth-first search, recursion, backtracking
 
 /**
  * @param {character[][]} board
@@ -7,7 +7,58 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
+    const start = word[0];
+    const m = board.length;
+    const n = board[0].length;
 
+    // search board for first character in word
+    for (let row = 0; row < m; row++) {
+        for (let col = 0; col < n; col++) {
+            if (board[row][col] === start) {
+                // DFS to find subsequent characters in word
+                if (searchAdjacent(row, col, 0)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    // DFS helper function
+    function searchAdjacent(row, col, index) {
+        // full length of word has been found
+        if (index === word.length) {
+            return true;
+        }
+
+        // check coordinate is in-bounds
+        if (row < 0 || row >= m || col < 0 || col > n) {
+            return false;
+        }
+
+        // check current character matches supplied index of word
+        if (board[row][col] !== word[index]) {
+            return false;
+        }
+
+        const nextIndex = index+1;
+
+        return searchAdjacent(row-1, col, nextIndex) // above
+            || searchAdjacent(row, col+1, nextIndex) // right
+            || searchAdjacent(row+1, col, nextIndex) // below
+            || searchAdjacent(row, col-1, nextIndex);// left
+    }
+
+    // word not found in board
+    return false;
+
+    // TODO: fails for the following test case:
+        // board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+        // word = "ABCB"
+        // returns true, when it should return false
+    // this is because it's going back to the B to its left, which is invalid as it would be visited twice
+        // we need to prevent re-searching elements on the path
+            // we can't modify the board as that ruins the recursive backtracking
+            // TODO: we could pass along an additional data structure of all the current elements on the path?
 };
 
 // given an `m x n` grid of characters `board`, and a string `word`:
