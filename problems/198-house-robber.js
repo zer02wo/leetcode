b4cddb46565a71ad71003c8df2ee3d91f0e5a463
zer02wo <stephenstubbsbeta@gmail.com>
2025-08-14 10:54:43 +0100

    [198] Initial problem thoughts/walkthrough

--- problems/198-house-robber.js
+++ problems/198-house-robber.js
@@ -1,4 +1,84 @@
 // https://leetcode.com/problems/house-robber/
+// tags: medium, array
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+
+};
+
+// given integer array `nums` representing the money at each house on a street
+// return the maximum amount of money you can "rob" without "alerting the police"
+    // adjacent houses have security systems connected
+    // police will automatically be alerted if two adjacent houses were broken into on the same night
+
+// EXAMPLE: nums = [1,2,3,1]
+// OUTPUT: 4
+    // nums[0] + nums[2] = 4
+    // nums[1] + nums[3] = 3
+    // nums[0] + nums[3] = 2
+
+// EXAMPLE: nums = [2,7,9,3,1]
+// OUTPUT: 12
+    // nums[0] + nums[2] + nums[4] = 12
+
+// EXAMPLE: nums = [2,1,1,2]
+// OUTPUT: 4
+    // nums[0] + nums[3] = 4
+    // i.e. sometimes worth to skip two consecutive houses
+
+// constraints:
+    // 1 <= nums.length <= 100
+    // 0 <= nums[i] <= 400
+
+// no immediate/obvious intuition, lets try to break this down:
+// if we take nums[i], this means we cannot take nums[i+1] or nums[i-1]
+// so we have two decisions:
+    // rob current house
+    // don't rob current house
+
+// EXAMPLE WALKTHROUGH: nums = [2,7,9,3,1]
+// Rob nums[0]      = 2
+// NoRob nums[0]    = 0
+
+// Rob nums[1]      = 7
+// NoRob nums[1]    = 2
+    // because we could rob the previous house (nums[0]) instead
+
+// Rob nums[2]      = 11
+    // because we could rob this *and* nums[0]
+// NoRob nums[2]    = 7
+    // because we could rob the previous house (nums[1]) instead
+
+// Rob nums[3]      = 10
+    // because we could rob this *and* nums[1]
+// NoRob nums[3]    = 11
+    // because we could rob the previous house (nums[2])
+        // *and* that allows us to rob nums[0]
+
+// Rob nums[4]      = 12
+    // because we could rob this *and* nums[2] *and* nums[0]
+// NoRob nums[4]    = 11
+    // because we could rob the previous house (nums[3])
+        // *and* that allows us to rob nums[1]
+
+// at each of these steps we would want to record the maximum we could earn:
+// nums = [2, 7,  9,  3,  1]
+// loot = [2, 7, 11, 11, 12]
+
+// recurrence relationship:
+//               (NoRob)            (Rob)
+// loot[i] = max(loot[i-1], (loot[i-2] + nums[i]))
+
+// referenced this video for explanation on principle, not viewed code:
+    // https://www.youtube.com/watch?v=kIII1uT6F8Y
+
+
+
+// ----- OLD / FAILED FIRST ATTEMPT BELOW LINE -----
+
+// https://leetcode.com/problems/house-robber/
 // tags: medium, leetle
 
 /**
